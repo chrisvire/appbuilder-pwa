93c9a458876d2566ec7129691e4ec74e35474501
Chris Hubbard <chris@thehubbards.org>
2025-04-04 11:40:15 -0400

    Add Tabs

--- src/routes/songs/[collection]/[book]/+page.svelte
+++ src/routes/songs/[collection]/[book]/+page.svelte
@@ -4,7 +4,7 @@
     import ChapterSelector from '$lib/components/ChapterSelector.svelte';
     import Navbar from '$lib/components/Navbar.svelte';
     import contents from '$lib/data/contents';
-    import { contentsStack } from '$lib/data/stores';
+    import { contentsStack, convertStyle, s, t } from '$lib/data/stores';
     import { getRoute } from '$lib/navigate';
 
     function backNavigation() {
@@ -15,6 +15,8 @@
     }
     $: showBackButton =
         contents?.features?.['navigation-type'] === 'up' && $contentsStack.length > 0;
+
+    $: selectedTab = contents?.features?.['song-number-layout'] ? 'numbers' : 'songs';
 </script>
 
 <div class="grid grid-rows-[auto,1fr]" style="height:100vh;height:100dvh;">
@@ -27,5 +29,35 @@
                 </div>
             {/snippet}
         </Navbar>
+    </div>
+
+    <div class="overflow-y-auto mx-auto md:max-w-screen-md w-full">
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <div
+            role="tablist"
+            class="dy-tabs dy-tabs-bordered"
+            style={convertStyle($s['ui.song.tabs'])}
+        >
+            <!-- svelte-ignore a11y_no_static_element_interactions -->
+            <div
+                name="tabs_1"
+                class="dy-tab dy-tab-bordered {selectedTab === 'numbers' ? 'dy-tab-active' : ''}"
+                on:click={() => (selectedTab = 'numbers')}
+                aria-label="by number tab"
+                style={convertStyle($s['ui.song.number.text'])}
+            >
+                <span class="dy-tab-text">{$t['Song_List_By_Number']}</span>
+            </div>
+            <!-- svelte-ignore a11y_no_static_element_interactions -->
+            <div
+                name="tabs_1"
+                class="dy-tab dy-tab-bordered {selectedTab === 'titles' ? 'dy-tab-active' : ''}"
+                on:click={() => (selectedTab = 'titles')}
+                aria-label="by title tab"
+                style={convertStyle($s['ui.song.title.text'])}
+            >
+                <span class="dy-tab-text">{$t['Song_List_By_Title']}</span>
+            </div>
+        </div>
     </div>
 </div>
